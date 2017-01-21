12d2ce863f4aa4763fa5d788dac102e3dd51b936
Darren Burgess <darrentburgess@gmail.com>
2017-01-21 11:00:36 -0500

    add rott-13 function

--- rott-13.js
+++ rott-13.js
@@ -1,13 +1,47 @@
 function rott13(string) {
+  var result = '';
+  var length = string.length;
+  var limit;
+  var code;
 
+  function isLetter(chr) {
+    return chr.toLowerCase() !== chr.toUpperCase();
+  }
+
+  function letterTypeLimit(chr) {
+    if (chr >= "A" && chr <= "Z") {
+      return 90;
+    } else if (chr >= 'a' && chr <= 'z') {
+      return 122;
+    }
+  }
+
+  for (i = 0; i < length; i++) {
+    limit = letterTypeLimit(string[i]) - 13; 
+    code = string[i].charCodeAt(0);
+
+    if (code < limit) {
+      newChar = String.fromCharCode(code + 13);
+    } else {
+      newChar = String.fromCharCode(code - 13);
+    }
+
+    if (isLetter(newChar)) {
+      result += newChar;
+    } else {
+      result += string[i];
+    }
+  }
+
+  return result;
 }
 
-function rott13Test(string) {
-  string2 = rott13(rott13(string))
+function rottTest() {
+  string = 'Teachers open the door, but you must enter by yourself.';
 
-  if (string === string2) {
-    return true;
+  if (string === rott13(rott13(string))) {
+    return 'PASSED';
   } else {
-    return false;
+    return "FAILED. Return value:\n    '" + rott13(string) + "'\nshould be\n    'Grnpuref bcra gur qbbe, ohg lbh zhfg ragre ol lbhefrys.'";
   }
 }
