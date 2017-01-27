ab0390c7f7a1dd831a979a70c5f416a68d39bd27
Darren Burgess <darrentburgess@gmail.com>
2017-01-26 20:35:38 -0500

    js date and time exercises

--- object_notes.md
+++ object_notes.md
@@ -1,5 +1,6 @@
 # Objects in Javascript
 
+
   * JS comes with a set of built in objects
       * String, Array, Object, Math, Date
       * JS converts primitives to objects when you us an object method on them
@@ -37,3 +38,38 @@
   * inerating objects
       * use `for (someVar in objectName) { //do stuff }`
   * `object.keys()` will return an array of the name of all the properties
+  * Objects vs Arrays
+      * what are the use cases for when to use a object or and array
+      * arrays
+          * arrays are great for lists of things
+          * even lists of things that are different types
+          * and then performing operations on those items by iterating
+          * or removing and adding items
+          * arrays maintain a specific order
+      * objects
+          * use objects when you need to represent something that has attributes
+          * access generally is not via iteration, but by keyed access
+          * objects are sometimes called associative arrays
+              * keys are associated with specific data
+          * so use objects when access by name is required
+      * now arrays are objects, but you should not really use object methods
+          * array methods are more idiomatic to arrays
+              * use `splice` instead of `delete`
+              * use `Array.forEach` instead of `for .. in`
+  * Mutability
+      * js has both primitive types: number, string, boolean, null, undefined
+      * and objects
+      * primitive types are immutable. This means that if you change the
+        value of a variable set to a primitive, a new value is created in memory
+      * objects on the other hand are mutable. They can be changed and the are still
+        the same object. The data (name/value pairs) can be changed.
+  * Pure Functions and Side Effects
+      * functions modify exteral values
+          * like variables in the outer scope
+          * objects can be mutated
+          * this is called `function side effects`
+      * pure functions have no side effects
+      * pure functions use only their arguments to determine the return value
+      * pure functions always resolve to the same value, give same arguments
+      * consider if a functions needs to be pure or have side effects
+
--- objects.js
+++ objects.js
@@ -58,3 +58,105 @@ function wordCount(str) {
 
   return wordCount;
 }
+
+// Math object functions
+function radiansToDegrees(radians) {
+  return radians / ( Math.PI / 180 );
+}
+
+function logPositive() {
+  var x = -180;
+  console.log(Math.abs(x));
+}
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getDayNumber() {
+  var dayNumber = new Date;
+  return "today is day number " + dayNumber.getDay();
+}
+
+function dayOfWeek() {
+  var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  var today = new Date();
+
+  return daysOfWeek[today.getDay()];
+}
+
+function formatMonth(date) {
+  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  var month = date.getMonth();
+  
+  return months[month] + " ";
+}
+
+function getDayName(date) {
+  var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  return daysOfWeek[date.getDay()] + ", ";
+}
+
+function formatDay(date) {
+  var day = date.getDate();
+  var remainder = day % 10;
+
+  if (day >= 11 && day <= 13) {
+    suffix = "th";
+  } else if (remainder === 1) {
+    suffix = "st";
+  } else if (remainder === 2) {
+    suffix = "nd";
+  } else if (remainder === 3) {
+    suffix = "rd";
+  } else {
+    suffix = "th"
+  }
+  
+  return day.toString() + suffix;
+}
+
+function formattedDate(date) {
+  var dayName = getDayName(date);
+  var month = formatMonth(date);
+  var day = formatDay(date);
+
+  return "Today's date is " + dayName + month + day;
+}
+
+function getTomorrow() {
+  var tomorrow = new Date(); 
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  return formattedDate(tomorrow);
+}
+
+function compareDates(date1, date2) {
+  if (date1.toDateString() === date2.toDateString()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function formatTime() {
+  var today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+
+  function formatNumber(number) {
+    if (number < 10) {
+      number = "0" + number.toString();
+    } else {
+      number.toString();
+    }
+
+    return number;
+  }
+
+  return formatNumber(hours) + ":" + formatNumber(minutes);
+}
+
