f37ca845ed459db210ce5d09a75bcc7ed548b8c4
Darren Burgess <darrentburgess@gmail.com>
2017-02-06 15:04:09 -0500

    add many javascript exercises

---
+++ js_language_fundamentals.js
@@ -0,0 +1,275 @@
+var myBoolean = true;
+var myString = 'hello';
+var myArray = [];
+var myOtherString = '';
+
+if (myBoolean) {
+  console.log('Hello');
+}
+
+if (!myString) {
+  console.log('World');
+}
+
+if (!!myArray) {
+  console.log('World');
+}
+
+if (myOtherString || myArray) {
+  console.log('!');
+}
+
+// what is logged at 7, 11, 15, 19
+/*
+In this program, we initialize four variables to four
+different values, a boolean, string, empty array and an
+empty string. In the first conditional statement, we check
+if the value of the boolean is true. true is true and so the program logs `Hello`. In the next condition, !myString coerces to false. Nothing is logged as line 11 does not run.
+The next condition coerces myArray to the boolean value true.
+This essentially reads as `not not myArray` Since an empty array object is truthy, this expression evaluates to true and the program logs 'World'. In the last condition, we use the logical or operator to examine if either value is truthy. If either is truthy, then the condition evaluates to true. In this case myArray is truthy as again, empty array objects are truthy. The program logs '!'.
+*/
+
+function numbersMath() {
+  function forceNumberInput(count) {
+    var input;
+    do {
+      input = prompt('Enter the ' + count + ' number:');
+    } while (isNaN(input) || input === '');
+
+    return Number(input);
+  }
+
+  var number1 = forceNumberInput('first');
+  var number2 = forceNumberInput('second');
+
+  console.log(number1 + ' + ' + number2 + ' = ' + (number1 + number2));
+  console.log(number1 + ' - ' + number2 + ' = ' + (number1 - number2));
+  console.log(number1 + ' * ' + number2 + ' = ' + (number1 * number2));
+  console.log(number1 + ' / ' + number2 + ' = ' + Math.floor(number1 / number2));
+  console.log(number1 + ' % ' + number2 + ' = ' + (number1 % number2));
+  console.log(number1 + ' ** ' + number2 + ' = ' + Math.pow(number1, number2));
+}
+
+function stringLength() {
+  var input;
+  do {
+    input = prompt('Enter a string:');
+  } while (input === '');
+
+  if (input === null) {
+    console.log("User cancelled input");
+  } else {
+    console.log('There are ' + input.length + ' characters in "' + input + '"');
+  }
+}
+
+function stringLength2() {
+  var input;
+  var newStr;
+
+  do {
+    input = prompt('Enter a string:');
+  } while (input === '');
+
+  if (input === null) {
+    console.log('User cancelled input');
+  } else {
+    newStr = input.replace(' ', '');
+    console.log('There are ' + newStr.length + ' non-space characters in "' + input + '"');
+  }
+}
+
+function stringLengthAlpha() {
+  var input;
+  var newStr
+
+  do {
+    input = prompt('Enter a string');
+  } while (input === '');
+
+  if (input === null) {
+    console.log('user canclled');
+  } else {
+    newStr = input.replace(/[^A-z]+/, '');
+    console.log('There are ' + newStr.length + ' alpha characters in "' + input + '"');
+  }
+}
+
+function stringToInteger(str) {
+  var result = 0;
+  str = str.split('').reverse().join('');
+
+  for (i = 0; i < str.length; i += 1) {
+    result += str.charAt(i) * Math.pow(10, i);
+  }
+
+  return result;
+}
+
+function stringToInteger2(str) {
+  var numbers = {
+    0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9 
+  };
+
+  var result = 0;
+  var number;
+
+  str = str.split('').reverse().join('');
+
+  for (i = 0; i < str.length; i +=1 ) {
+    number = numbers[str[i]];
+    result += number * Math.pow(10, i);
+  }
+
+  return result;
+}
+
+function stringToInteger3(str) {
+  var numbers = {
+    0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9 
+  }
+
+  var result = 0;
+  var number;
+
+  str = str.split('').reverse().join('');
+
+  for (i = 0; i < str.length; i += 1) {
+    number = numbers[str[i]];
+
+    if (str[i] === '-') {
+      result = -result;
+    } else if (!isNaN(number)) {
+      result += number * Math.pow(10, i);
+    }
+  }
+
+  return result;
+}
+
+function stringToSignedInteger(str) {
+  var numbers = {
+    0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9 
+  }
+
+  var result = 0;
+  var number;
+
+  result = str.split('').reverse().map(function(chr, idx) {
+    if (!isNaN(chr)) {
+      return numbers[chr] * Math.pow(10, idx);
+    } else {
+      return null;
+    }
+  }).reduce(function(a, b) { return a + b; }, 0);
+
+  if (str.slice(0, 1) === '-') {
+    return -result;
+  }
+
+  return result;
+}
+
+function integerToString(int) {
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var remainder;
+  var result = '';
+
+  do {
+    remainder = int % 10;
+    result = numbers[remainder] + result;
+    int = Math.floor(int / 10);
+  } while (int > 0);
+
+  return result;
+}
+
+function signedIntegerToString(int) {
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var remainder;
+  var result = '';
+  var num = int;
+  
+  if (int < 0) {
+    num = -int;
+  }
+
+  do {
+    remainder = num % 10;
+    result = numbers[remainder] + result;
+    num = Math.floor(num / 10);
+  } while (num > 0);
+
+  if (int < 0) {
+    return '-' + result;
+  } else {
+    return result;
+  }
+}
+
+function arrayCopy1(arr) {
+  result = [];
+
+  for (var i = 0; i < arr.length; i += 1) {
+    result.push(arr[i]);
+  }
+
+  return result;
+}
+
+function arrayCopy2(arr) {
+  result = [];
+  arr.forEach(function(e) {
+    result.push(e);
+  });
+  return result;
+}
+
+function arrayCopy3(arr) {
+  return arr.map(function(e){
+    return e;
+  });
+}
+
+function concat(array1, value) { 
+ result = array1.slice();
+
+  if (Array.isArray(value)) {
+    value.forEach(function(e) {
+      result.push(e);
+    });
+  } else {
+    result.push(value);
+  }
+
+  return result;
+}
+
+function concat2(arr) {
+  // guard clause when first argument is not an array 
+  if (! Array.isArray(arr)) {
+    return 'error: ' + arr + 'is not an array';
+  }
+
+  // create a result array from arr
+  result = arr.slice();
+
+  // parse the arguments into args array, removing first argument (the original array)
+  args = Array.prototype.slice.call(arguments).splice(1);
+
+  // parse the arguments
+  for (i = 0; i < args.length; i += 1) {
+    arg = args[i];
+
+    // push each value of arg when arg is an array, otherwise just push
+    if (Array.isArray(arg)) {
+      args[i].forEach(function(e) {
+        result.push(e);
+      });
+    } else {
+      result.push(arg);
+    }
+  }
+
+  return result; 
+}
