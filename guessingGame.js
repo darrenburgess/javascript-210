19bb58bf2ba38f792409239019c4c53ecbbde71f
Darren Burgess <darrentburgess@gmail.com>
2017-05-09 19:54:10 -0400

    guessing game version 3

--- guessingGame.js
+++ guessingGame.js
@@ -1,68 +1,70 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  var answer = getRandomNumber(100);
-  var form = document.getElementsByTagName('form')[0];
+// create event handler for DOMContentLoaded
+// create answer number as random number from 1 to 100
+// create event listener on form submission, use preventDefault
+// store user guess in variable
+// validate the guess as number
+// create variable to store string for message to display
+// check if guess is a number
+// add a count of guesses 
+// check guess against answer and display appropriate message
+// test guessing
+// create event listener for new game link
+// extra: disable button on correct answer. also disable input. Grey out button.
+
+document.addEventListener('DOMContentLoaded', function() {
+  var form = document.querySelector('form');
   var input = form.querySelector('input');
-  var messageParagraph = document.querySelector('p');
-  var newGameLink = document.querySelector('a');
-  var submitButton = document.querySelector('[type="submit"]');
+  var message = document.querySelector('p');
+  var newGameLink = document.querySelector('a')
+
+  var answer;
+  var count;
   var guess;
-  var guessCount;
   var message;
 
   function getRandomNumber(limit) {
-    return Math.floor(Math.random() * limit) + 1;
+    return Math.floor(Math.random() * 100 + 1);
   }
 
   function checkGuess(guess, answer) {
     var msg;
-   
+
     if (isNaN(guess)) {
-      msg = 'Guess must be a number';
+      msg = 'Please enter a number for your guess';
     } else if (guess < answer) {
-      msg = 'Guess was less than my answer';
+      msg = 'Your guess ' + guess + ' is less than my answer';
     } else if (guess > answer) {
-      msg = 'Guess was greater than my answer';
-    } else if (guess === answer) {
-      msg = 'You guessed ' + guess + ' correctly in ' + guessCount + ' attempts';
-      toggleSubmitButton();
-    }
-
-    if (!isNaN(guess)) guessCount += 1;
-
-    messageParagraph.textContent = msg;
-  }
-
-  function toggleSubmitButton(on) {
-    if (submitButton.disabled === true || on) {
-      submitButton.disabled = false;
-      input.disabled = false;
-      input.focus();
-      submitButton.style.background = 'linear-gradient(to bottom, #CC183E 0%, #780E24 100%)';
+      msg = 'Your guess ' + guess + ' is more than my answer';
     } else {
-      submitButton.disabled = true;
-      input.disabled = true;
-      submitButton.style.background = 'grey';
+      msg = 'You guessed ' + guess + ' correctly in ' + count + ' tries!';
+    }
+
+    if (!isNaN(guess)) {
+      message.textContent = msg;
+      count += 1;
     }
   }
 
-  function newGame(event) {
-    answer = getRandomNumber(100);
-    guessCount = 1;
-    input.focus();
-    messageParagraph.textContent = 'Guess a number between 1 and 100';
-    toggleSubmitButton(true);
-  }
-
-  function submitAnswer(event) {
-    event.preventDefault();
-    guess = parseInt(input.value);
-    checkGuess(guess, answer);
+  function clearInput() {
     input.value = '';
     input.focus();
   }
-  
-  form.addEventListener('submit', submitAnswer);
+
+  function submitGuess(event) {
+    event.preventDefault();
+    guess = input.value;
+    checkGuess(guess, answer);
+    clearInput();
+  }
+
+  function newGame(event) {
+    count = 1;
+    answer = getRandomNumber(100);
+    clearInput();
+  }
+
   newGameLink.addEventListener('click', newGame);
+  form.addEventListener('submit', submitGuess);
 
   newGame();
 });
