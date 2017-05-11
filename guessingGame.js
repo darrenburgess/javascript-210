2bfcd8dd1b9b52fd892f3752c1e533187faede5b
Darren Burgess <darrentburgess@gmail.com>
2017-05-10 22:05:04 -0400

    guessing game final

--- guessingGame.js
+++ guessingGame.js
@@ -1,4 +1,4 @@
-// create event handler for DOMContentLoaded
+// create event listener for DOMContentLoaded
 // create answer number as random number from 1 to 100
 // create event listener on form submission, use preventDefault
 // store user guess in variable
@@ -13,35 +13,71 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var form = document.querySelector('form');
-  var input = form.querySelector('input');
-  var message = document.querySelector('p');
-  var newGameLink = document.querySelector('a')
-
+  var input = document.querySelector('input');
+  var message = document.getElementsByTagName('p')[0];
+  var newGameLink = document.querySelector('a');
+  var guessButton = form.querySelector('[type="submit"]')
   var answer;
-  var count;
   var guess;
-  var message;
+  var count;
 
-  function getRandomNumber(limit) {
-    return Math.floor(Math.random() * 100 + 1);
+  newGame();
+
+  form.addEventListener('submit', onSubmitAnswer);
+  newGameLink.addEventListener('click', newGame);
+
+  function newGame() {
+    answer = Math.floor(Math.random() * 100) + 1;
+    count = 1;
+    toggleInputs(true);
+    clearInput();
+    message.textContent = 'Guess a number between 1 and 100';
+  }
+
+  function onSubmitAnswer(event) {
+    event.preventDefault;
+    guess = parseInt(input.value); 
+    message.textContent = checkGuess(guess, answer);
+    checkGameReset(guess, answer);
   }
 
   function checkGuess(guess, answer) {
     var msg;
 
     if (isNaN(guess)) {
-      msg = 'Please enter a number for your guess';
+      msg = 'Please enter a number';
     } else if (guess < answer) {
-      msg = 'Your guess ' + guess + ' is less than my answer';
+      msg = 'Your guess is less than my answer';
     } else if (guess > answer) {
-      msg = 'Your guess ' + guess + ' is more than my answer';
+      msg = 'Your guess is greater than my answer';
     } else {
-      msg = 'You guessed ' + guess + ' correctly in ' + count + ' tries!';
+      msg = 'Correct! You guessed ' + answer + ' in ' + count + ' tries!';
     }
 
-    if (!isNaN(guess)) {
-      message.textContent = msg;
+    return msg;
+  }
+
+  function checkGameReset(guess, answer) {
+    if (guess === answer) {
+      toggleInputs(false);
+    } else if (!isNaN(guess)) {
       count += 1;
+      clearInput();
+    }
+  }
+
+  function toggleInputs(initializeGame) {
+    if (guessButton.disabled || initializeGame) {
+      guessButton.disabled = false;
+      input.disabled = false;
+      guessButton.classList.remove('button-grey');
+      guessButton.classList.add('button-red');
+    } else {
+      guessButton.disabled = true;
+      input.disabled = true;
+      guessButton.classList.add('button-grey');
+      guessButton.classList.remove('button-red');
+      newGameLink.focus();
     }
   }
 
@@ -49,22 +85,4 @@ document.addEventListener('DOMContentLoaded', function() {
     input.value = '';
     input.focus();
   }
-
-  function submitGuess(event) {
-    event.preventDefault();
-    guess = input.value;
-    checkGuess(guess, answer);
-    clearInput();
-  }
-
-  function newGame(event) {
-    count = 1;
-    answer = getRandomNumber(100);
-    clearInput();
-  }
-
-  newGameLink.addEventListener('click', newGame);
-  form.addEventListener('submit', submitGuess);
-
-  newGame();
 });
